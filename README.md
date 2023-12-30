41692c63ae8d413e231404ac649c09bd20629e82
smp <smpurcell@bwh.harvard.edu>
2023-12-30 16:04:40 -0500

    lunapi step1

--- README.md
+++ README.md
@@ -1,2 +1,78 @@
 # luna-api
+
 Luna API &amp; Python bindings
+
+## Working notes
+
+Assumptions:
+
+ - luna is installed and `libluna.so` is a system library folder (e.g. on Linux, with `sudo` privileges `cp` to `/usr/local/lib` and then `ldconfig`) 
+
+ - LightGBM, FFTW and zlib are similarly available system-wide
+
+ - modify `-L` paths below or add `-Wl,-rpath` etc as needed
+ 
+
+### C/C++
+
+
+```
+g++ -O2 -std=gnu++17 example.cpp -o example -I../luna-base -lluna -lz -lfftw3 -l_lightgbm
+```
+
+Run, assuming `learn-nsrr01.edf` and `learn-nsrr01-profusion.xml` are in the current folder:
+
+```
+./example
+```
+
+### Python
+
+To build the Python module for lunapi using pybind11
+
+```
+pip install pybind11
+```
+
+```
+g++ -O3 -Wall -shared -std=c++17 -fPIC \
+     -I../luna-base/ -I../luna-base/stats \
+     $(python3 -m pybind11 --includes) \
+     -Wno-sign-compare \
+     `python3-config --ldflags` \
+     lunapi.cpp \
+     -o lunapi$(python3-config --extension-suffix) \
+     -lluna -l_lightgbm -lfftw3 -lz
+```
+
+### Docker
+
+
+```
+docker build -t lunapi .
+```
+
+```
+docker run -it --rm  --user root -e GRANT_SUDO=yes -p 8888:8888 -v "${PWD}":/home/jovyan/work quay.io/jupyter/datascience-notebook
+```
+
+```
+docker run -it --rm -p 8888:8888 -v "${PWD}":/lunapi lunapi
+```
+
+
+### misc notes
+
+
+```
+pip install pybind11
+```
+
+
+```
+g++ -O3 -Wall -shared -std=c++17 -fPIC      -I/build/luna-base/ -I/build/luna-base/stats      $(python3 -m pybind11 --includes)      -Wno-sign-compare      `python3-config --ldflags`      lunapi.cpp      -o lunapi$(python3-config --extension-suffix)      -lluna -l_lightgbm -lfftw3 -lz
+```
+
+
+
+
---
+++ lunapi.cpp
@@ -0,0 +1,72 @@
+
+//    --------------------------------------------------------------------
+//
+//    This file is part of Luna.
+//
+//    LUNA is free software: you can redistribute it and/or modify
+//    it under the terms of the GNU General Public License as published by
+//    the Free Software Foundation, either version 3 of the License, or
+//    (at your option) any later version.
+//
+//    Luna is distributed in the hope that it will be useful,
+//    but WITHOUT ANY WARRANTY; without even the implied warranty of
+//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//    GNU General Public License for more details.
+//
+//    You should have received a copy of the GNU General Public License
+//    along with Luna. If not, see <http://www.gnu.org/licenses/>.
+//
+//    Please see LICENSE.txt for more details.
+//
+//    --------------------------------------------------------------------
+
+#include <pybind11/pybind11.h>
+#include <pybind11/stl.h> 
+#include <pybind11/eigen.h>
+
+#include "luna.h"
+
+namespace py = pybind11;
+
+PYBIND11_MODULE(lunapi, m) {
+
+  m.doc() = "LunaAPI : Python bindings to the C/C++ Luna library";         
+
+  m.def("init", &lunapi_t::init, "Initiate the Luna library" );
+    
+  py::class_<lunapi_t>(m, "new")
+    .def(py::init<const std::string &>())
+    .def( "attach_edf",&lunapi_t::attach_edf,"Attach an EDF" )
+    .def( "attach_annot",&lunapi_t::attach_annot,"Attach an annotation file to the current EDF" )
+    
+    .def( "refresh", &lunapi_t::refresh,"Reattach the current EDF")
+    .def( "drop", &lunapi_t::refresh,"Drop the current EDF" )
+    .def( "clear" , &lunapi_t::clear,"Clear all variables" )
+
+    .def( "channels", &lunapi_t::channels,"Return a list of channel labels" )
+    .def( "annots", &lunapi_t::annots,"Return a list of annotation class labels" )
+    .def( "stat" , &lunapi_t::status,"Return a map/dict of key details on the current EDF" )
+
+    .def( "e2i" , &lunapi_t::epochs2intervals , "Convert epoch numbers to interval tuples" )
+    .def( "s2i" , &lunapi_t::seconds2intervals , "Convert second (start/stop tuples) to interval tuples" )
+        
+    .def( "slice",&lunapi_t::slice,"Return a data matrix/column header tuple" )
+    .def( "slices",&lunapi_t::slices,"Return a list of data matrices (one per epoch/interval)" )
+
+    .def_static("var",py::overload_cast<const std::string &,const std::string &>(&lunapi_t::var),"Set a variable value")
+    .def_static("var",py::overload_cast<const std::string &>(&lunapi_t::var),"Show a variable value")
+    .def_static("vars",&lunapi_t::vars,"List several variables")
+    .def_static("dropvar",&lunapi_t::dropvar,"Drop a variable")
+    .def_static("dropvars",&lunapi_t::dropvars,"Drop several variables")
+   
+    .def("eval",&lunapi_t::eval,"Evaluate an arbitrary set of Luna commands given an attacged EDF" )
+    .def("commands",&lunapi_t::commands,"List commands resulting from a prior eval()" )
+    .def("strata",&lunapi_t::strata,"List command/strata tuples resulting from a prior eval()" )
+    .def("variables",&lunapi_t::variables,"List variables (columns) from a table (defined by a command/strat pair) from a prior eval()")
+    .def("data",&lunapi_t::data , "Return table (defined by a command/strata pair) from a prior eval()" )
+    
+    .def("__repr__",[](const lunapi_t & a) { return "<lunapi-instance>"; });
+
+    
+}
+
