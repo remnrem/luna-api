32020f2473359750dacda5bc6f3a957e4de00734
Shaun Purcell <smpurcell@bwh.harvard.edu>
2024-01-11 19:54:37 -0500

    arm64 pull libs test

---
+++ build-macos-arm64.sh
@@ -0,0 +1,9 @@
+mkdir depends
+# get libs
+wget -O macos-arm64.tar.gz "https://www.dropbox.com/scl/fi/zg2itwsoc8bo0018w969n/macos-arm64.tar.gz?rlkey=3lj8v9ziig9fggs8fz928d80a&dl=0"
+tar xzvf macos-arm64.tar.gz
+cp macos-arm64/*.a depends/
+
+# luna-base include headers
+cd depends
+git clone https://github.com/remnrem/luna-base.git
