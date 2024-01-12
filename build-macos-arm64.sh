2cad63dfb622035743169e15ec330891a5dabbe1
Shaun Purcell <smpurcell@bwh.harvard.edu>
2024-01-12 15:14:42 -0500

    lz

--- build-macos-arm64.sh
+++ build-macos-arm64.sh
@@ -1,7 +1,7 @@
 
 mkdir depends
 # get libs
-wget -O macos-arm64.tar.gz "https://www.dropbox.com/scl/fi/zg2itwsoc8bo0018w969n/macos-arm64.tar.gz?rlkey=3lj8v9ziig9fggs8fz928d80a&dl=0"
+wget -O macos-arm64.tar.gz "https://www.dropbox.com/scl/fi/7pii8r7hfkvtr8ufr0spa/macos-arm64.tar.gz?rlkey=6je0i913hk613x0fjx81ibwxm&dl=0"
 tar xzvf macos-arm64.tar.gz
 cp macos-arm64/*.a depends/
 
