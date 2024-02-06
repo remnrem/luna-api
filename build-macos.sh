ded1b75fa7c1b44038df45f8be18f9cd33aaa6f6
remnrem <remnrem@gmail.com>
2024-02-06 12:20:45 -0500

    v0.0.5

--- build-macos.sh
+++ build-macos.sh
@@ -1,34 +1,31 @@
 
+cd ${GITHUB_WORKSPACE}
 mkdir depends
-cd depends
-
-# libomp
-cp /usr/local/opt/libomp/lib/libomp.a .
 
 # FFTW
+cd ${GITHUB_WORKSPACE}/depends
 wget https://www.fftw.org/fftw-3.3.10.tar.gz
 tar -xzvf fftw-3.3.10.tar.gz 
 cd fftw-3.3.10
-./configure
-make -j4
+./configure --with-pic
+make -j4  CFLAGS=-fPIC
 make install
-cp .libs/libfftw3.a ../
-cd ..
 
 # LightGBM
+cd ${GITHUB_WORKSPACE}/depends
 git clone --recursive https://github.com/microsoft/LightGBM
 cd LightGBM
 mkdir build
 cd build
-cmake -DBUILD_STATIC_LIB=ON ..
+cmake -DBUILD_STATIC_LIB=ON  -DCMAKE_POSITION_INDEPENDENT_CODE=ON -DUSE_OPENMP=OFF ..
 make -j4
-cd ..
-cp lib_lightgbm.a ../
-cd ..
 
 # luna-base
+cd ${GITHUB_WORKSPACE}/depends
 git clone https://github.com/remnrem/luna-base.git
 cd luna-base
 make -j4 ARCH=MAC LGBM=1 LGBM_PATH=../LightGBM/
-cp libluna.a ../
-cd ..
+cp libluna.a /usr/local/lib/
+
+
+ls -lrt /usr/local/lib
