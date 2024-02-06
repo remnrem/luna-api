eff78e870be3bf9d118d97b371d4ab01e79d11e2
remnrem <remnrem@gmail.com>
2024-02-06 10:17:42 -0500

    Refactor build-macos-arm64.sh build steps

--- build-macos-arm64.sh
+++ build-macos-arm64.sh
@@ -1,11 +1,11 @@
 
-cd ${GITHUB_WORKSPACE}
+mkdir /depends
+cd /depends
 
-mkdir depends
 # get libs: 
 wget -O macos-arm64.tar.gz "https://www.dropbox.com/scl/fi/5fe5tcbo7zvz5f1vrmtw9/macos-arm64.tar.gz?rlkey=ncn72rme3sami0dk538uhu52x&dl=0"
 tar xzvf macos-arm64.tar.gz
-cp macos-arm64/*.a depends/
+cp macos-arm64/*.a /depends/
 
 brew install fftw
 
@@ -13,7 +13,7 @@ pwd
 
 ls -l
 
-cd depends
+cd /depends
 
 # includes only
 # FFTW
@@ -21,13 +21,14 @@ wget https://www.fftw.org/fftw-3.3.10.tar.gz
 tar -xzvf fftw-3.3.10.tar.gz 
 
 # LightGBM
+cd /depends/
 git clone --recursive https://github.com/microsoft/LightGBM
 
 # luna-base
+cd /depends/
 git clone https://github.com/remnrem/luna-base.git
 cp libluna.a luna-base/
-cd ..
 
+cd /depends
 ls -l
-echo "here"
-ls -lR
+
