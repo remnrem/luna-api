19f924aeecef86d011ffd7693bf10db411112989
Shaun Purcell <smpurcell@bwh.harvard.edu>
2024-02-06 09:56:41 -0500

    m1

--- build-linux.sh
+++ build-linux.sh
@@ -4,8 +4,8 @@
 
 cd ${GITHUB_WORKSPACE}
 
-mkdir depends
-cd depends
+mkdir /depends
+cd /depends
 
 # FFTW
 curl -O https://www.fftw.org/fftw-3.3.10.tar.gz
@@ -14,10 +14,11 @@ cd fftw-3.3.10
 ./configure --with-pic
 make -j4 CFLAGS=-fPIC
 make install
-cp .libs/libfftw3.a ../
-cd ..
+ls -l .libs/
+cp .libs/libfftw3.a /depends/
 
 # LightGBM
+cd /depends/
 git clone --recursive https://github.com/microsoft/LightGBM
 cd LightGBM
 mkdir build
@@ -25,15 +26,14 @@ cd build
 cmake -DBUILD_STATIC_LIB=ON  -DCMAKE_POSITION_INDEPENDENT_CODE=ON -DUSE_OPENMP=OFF ..
 make -j4
 cd ..
-cp lib_lightgbm.a ../
-cd ..
+cp lib_lightgbm.a /depends/
 
 # luna-base
+cd /depends/
 git clone https://github.com/remnrem/luna-base.git
 cd luna-base
 make -j4 ARCH=MAC LGBM=1 LGBM_PATH=../LightGBM/
-cp libluna.a ../
-cd ..
+cp libluna.a /depends/
 
+ls -l /depends
 
-ls -lR
