ea5ad4b105f443d0db017620d68f5e0d29a55894
remnrem <remnrem@gmail.com>
2024-04-26 14:14:56 -0400

    0.0.6

--- build-linux.sh
+++ build-linux.sh
@@ -1,26 +1,10 @@
 
-#sudo apt-get update
-#sudo apt-get -y install wget libomp-dev
-
-echo "PWD = $PWD"
-
-ls -lrt
+cp CMakeLists.txt.GITHUB CMakeLists.txt
 
 cd ${GITHUB_WORKSPACE}
+mkdir depends
 
-echo "PWD = $PWD"
-
-echo "startting"
-
-ls -lrt
-cp CMakeLists.txt.LINUX CMakeLists.txt
-
-echo "building /depends"
-
-mkdir /depends
-cd /depends
-
-echo "done"
+cd ${GITHUB_WORKSPACE}/depends
 
 # FFTW
 curl -O https://www.fftw.org/fftw-3.3.10.tar.gz
@@ -30,10 +14,10 @@ cd fftw-3.3.10
 make -j4 CFLAGS=-fPIC
 make install
 ls -l .libs/
-cp .libs/libfftw3.a /depends/
+cp .libs/libfftw3.a ${GITHUB_WORKSPACE}/depends/
 
 # LightGBM
-cd /depends/
+cd ${GITHUB_WORKSPACE}/depends/
 git clone --recursive https://github.com/microsoft/LightGBM
 cd LightGBM
 mkdir build
@@ -44,14 +28,11 @@ cd ..
 cp lib_lightgbm.a /depends/
 
 # luna-base
-cd /depends/
+cd ${GITHUB_WORKSPACE}/depends/
 git clone https://github.com/remnrem/luna-base.git
 cd luna-base
 make -j4 ARCH=MAC LGBM=1 LGBM_PATH=../LightGBM/
+cp libluna.a ${GITHUB_WORKSPACE}/depends/
 
-echo "built libluna.a"
-ls -lrt
-cp libluna.a /depends/
-
-ls -l /depends
+ls -l ${GITHUB_WORKSPACE}/depends
 
