f4c02d3bf3b96eb4b754b82ac8a2d1c28609181c
Senthil Palanivelu <senthilcaesar@yahoo.co.in>
2024-11-14 15:31:58 -0500

    Update build-linux.sh

--- build-linux.sh
+++ build-linux.sh
@@ -7,6 +7,8 @@ mkdir ${GITHUB_WORKSPACE}/depends
 cd ${GITHUB_WORKSPACE}/depends
 
 # CMAKE
+sudo apt update
+sudo apt install -y wget
 wget https://cmake.org/files/v3.8/cmake-3.8.2.tar.gz
 tar -zxvf cmake-3.8.2.tar.gz
 cd cmake-3.8.2
