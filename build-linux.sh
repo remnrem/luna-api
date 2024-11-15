9742cc7b619ed21584952aa594ab3556a64cd337
remnrem <remnrem@gmail.com>
2024-11-15 15:19:28 -0500

    v2

--- build-linux.sh
+++ build-linux.sh
@@ -7,8 +7,8 @@ mkdir ${GITHUB_WORKSPACE}/depends
 cd ${GITHUB_WORKSPACE}/depends
 
 # CMAKE
-#sudo apt update
-#sudo apt install -y wget
+sudo apt update
+sudo apt install -y wget cmake
 #wget https://cmake.org/files/v3.8/cmake-3.8.2.tar.gz
 #tar -zxvf cmake-3.8.2.tar.gz
 #cd cmake-3.8.2
@@ -21,16 +21,6 @@ cd ${GITHUB_WORKSPACE}/depends
 #cp -r ./bin/* ${GITHUB_WORKSPACE}/
 #cp -r ./bin/* ${GITHUB_WORKSPACE}/depends/
 
-# FFTW
-cd ${GITHUB_WORKSPACE}/depends/
-curl -O https://www.fftw.org/fftw-3.3.10.tar.gz
-tar -xzvf fftw-3.3.10.tar.gz 
-cd fftw-3.3.10
-./configure --with-pic
-make -j4 CFLAGS=-fPIC
-make install
-ls -l .libs/
-cp .libs/libfftw3.a ${GITHUB_WORKSPACE}/depends/
 
 # LightGBM
 cd ${GITHUB_WORKSPACE}/depends/
@@ -42,6 +32,18 @@ cmake -DBUILD_STATIC_LIB=ON  -DCMAKE_POSITION_INDEPENDENT_CODE=ON -DUSE_OPENMP=O
 make -j4
 cd ..
 cp lib_lightgbm.a ${GITHUB_WORKSPACE}/depends/
+
+# FFTW
+cd ${GITHUB_WORKSPACE}/depends/
+curl -O https://www.fftw.org/fftw-3.3.10.tar.gz
+tar -xzvf fftw-3.3.10.tar.gz 
+cd fftw-3.3.10
+./configure --with-pic
+make -j4 CFLAGS=-fPIC
+make install
+ls -l .libs/
+cp .libs/libfftw3.a ${GITHUB_WORKSPACE}/depends/
+
 
 # luna-base
 cd ${GITHUB_WORKSPACE}/depends/
